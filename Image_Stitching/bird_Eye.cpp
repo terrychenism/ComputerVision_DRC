5bbe7ccf090227af778ec9780cfa70ce8af60587
terrychenism <hahakuku@hotmail.com>
2014-04-12 00:51:06 -0400

    Update bird_Eye.cpp

--- Image_Stitching/bird_Eye.cpp
+++ Image_Stitching/bird_Eye.cpp
@@ -115,97 +115,104 @@ void rotateImage(const Mat &input, Mat &output, double alpha, double beta, doubl
    Mat src, warp_dst, warp_rotate_dst,dst;
 
 
-   img = imread( "img_right_rectified.jpg", 1 );
-   
+   img = imread( "img_left_rectified.jpg", 1 );
+   //Mat img2 = imread( "./image/frame0.jpg", 1 );
+   //imshow("right",img2);
+
     double w = (double)img.cols;
     double h = (double)img.rows;
 	//double alpha = 0.5;
-int alpha_=90., beta_=45., gamma_=90.;
-int f_ = 500, dist_ = 500;
+	int alpha_=90., beta_=45., gamma_=90.;
+	int f_ = 500, dist_ = 500,dx_ = 0, dy_= 0;
 
-Mat destination;
-char* wndname1 = "Source image";
-char* wndname2 = "WarpPerspective: ";
+	Mat destination;
+	char* wndname1 = "Source image";
+	char* wndname2 = "WarpPerspective: ";
 
-string tbarname1 = "Alpha";
-string tbarname2 = "Beta";
-string tbarname3 = "Gamma";
-string tbarname4 = "f";
-string tbarname5 = "Distance";
-namedWindow(wndname1, 1);
-namedWindow(wndname2, 1);
-createTrackbar(tbarname1, wndname2, &alpha_, 180);
-createTrackbar(tbarname2, wndname2, &beta_, 180);
-createTrackbar(tbarname3, wndname2, &gamma_, 180);
-createTrackbar(tbarname4, wndname2, &f_, 2000);
-createTrackbar(tbarname5, wndname2, &dist_, 2000);
+	string tbarname1 = "Alpha";
+	string tbarname2 = "Beta";
+	string tbarname3 = "Gamma";
+	string tbarname4 = "f";
+	string tbarname5 = "Dz";
+	string tbarname6 = "Dx";
+	string tbarname7 = "Dy";
+	//namedWindow(wndname1, 1);
+	namedWindow(wndname2, 1);
+	createTrackbar(tbarname1, wndname2, &alpha_, 180);
+	createTrackbar(tbarname2, wndname2, &beta_, 180);
+	createTrackbar(tbarname3, wndname2, &gamma_, 180);
+	createTrackbar(tbarname4, wndname2, &f_, 2000);
+	createTrackbar(tbarname5, wndname2, &dist_, 2000);
+	createTrackbar(tbarname6, wndname2, &dx_, 2000);
+	createTrackbar(tbarname7, wndname2, &dy_, 2000);
 
-imshow(wndname1, img);
-while(true) {
-    double f, dist;
-    double alpha, beta, gamma;
-    alpha = ((double)alpha_ - 90.)*CV_PI/180;
-    beta = ((double)beta_ - 90.)*CV_PI/180;
-    gamma = ((double)gamma_ - 90.)*CV_PI/180;
-    f = (double) f_;
-    dist = (double) dist_;
+	//imshow(wndname1, img);
+	while(true) {
+		double f, dist, dx ,dy ;
+		double alpha, beta, gamma;
+		alpha = ((double)alpha_ - 90.)*CV_PI/180;
+		beta = ((double)beta_ - 90.)*CV_PI/180;
+		gamma = ((double)gamma_ - 90.)*CV_PI/180;
+		f = (double) f_;
+		dist = (double) dist_;
+		dx = (double)dx_;
+		dy = (double)dy_;
+		Size taille = img.size();
+		double w = (double)taille.width, h = (double)taille.height;
 
-    Size taille = img.size();
-    double w = (double)taille.width, h = (double)taille.height;
+		// Projection 2D -> 3D matrix
+		Mat A1 = (Mat_<double>(4,3) <<
+			1, 0, -w/2,
+			0, 1, -h/2,
+			0, 0,    0,
+			0, 0,    1);
 
-    // Projection 2D -> 3D matrix
-    Mat A1 = (Mat_<double>(4,3) <<
-        1, 0, -w/2,
-        0, 1, -h/2,
-        0, 0,    0,
-        0, 0,    1);
+		// Rotation matrices around the X,Y,Z axis
+		Mat RX = (Mat_<double>(4, 4) <<
+			1,          0,           0, 0,
+			0, cos(alpha), -sin(alpha), 0,
+			0, sin(alpha),  cos(alpha), 0,
+			0,          0,           0, 1);
 
-    // Rotation matrices around the X,Y,Z axis
-    Mat RX = (Mat_<double>(4, 4) <<
-        1,          0,           0, 0,
-        0, cos(alpha), -sin(alpha), 0,
-        0, sin(alpha),  cos(alpha), 0,
-        0,          0,           0, 1);
+		Mat RY = (Mat_<double>(4, 4) <<
+			cos(beta), 0, -sin(beta), 0,
+					0, 1,          0, 0,
+			sin(beta), 0,  cos(beta), 0,
+					0, 0,          0, 1);
 
-    Mat RY = (Mat_<double>(4, 4) <<
-        cos(beta), 0, -sin(beta), 0,
-                0, 1,          0, 0,
-        sin(beta), 0,  cos(beta), 0,
-                0, 0,          0, 1);
+		Mat RZ = (Mat_<double>(4, 4) <<
+			cos(gamma), -sin(gamma), 0, 0,
+			sin(gamma),  cos(gamma), 0, 0,
+			0,          0,           1, 0,
+			0,          0,           0, 1);
 
-    Mat RZ = (Mat_<double>(4, 4) <<
-        cos(gamma), -sin(gamma), 0, 0,
-        sin(gamma),  cos(gamma), 0, 0,
-        0,          0,           1, 0,
-        0,          0,           0, 1);
+		// Composed rotation matrix with (RX,RY,RZ)
+		Mat R = RX * RY * RZ;
 
-    // Composed rotation matrix with (RX,RY,RZ)
-    Mat R = RX * RY * RZ;
+		// Translation matrix on the Z axis change dist will change the height
+		Mat T = (Mat_<double>(4, 4) <<
+			1, 0, 0, dx,
+			0, 1, 0, dy,
+			0, 0, 1, dist,
+			0, 0, 0, 1);
 
-    // Translation matrix on the Z axis change dist will change the height
-    Mat T = (Mat_<double>(4, 4) <<
-        1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, dist,
-        0, 0, 0, 1);
+		// Camera Intrisecs matrix 3D -> 2D
+		Mat A2 = (Mat_<double>(3,4) <<
+			f, 0, w/2, 0,
+			0, f, h/2, 0,
+			0, 0,   1, 0);
 
-    // Camera Intrisecs matrix 3D -> 2D
-    Mat A2 = (Mat_<double>(3,4) <<
-        f, 0, w/2, 0,
-        0, f, h/2, 0,
-        0, 0,   1, 0);
+		// Final and overall transformation matrix
+		Mat transfo = A2 * (T * (R * A1));
 
-    // Final and overall transformation matrix
-    Mat transfo = A2 * (T * (R * A1));
+		// Apply matrix transformation
+		warpPerspective(img, destination, transfo, taille, INTER_CUBIC | WARP_INVERSE_MAP);
 
-    // Apply matrix transformation
-    warpPerspective(img, destination, transfo, taille, INTER_CUBIC | WARP_INVERSE_MAP);
-
-    imshow(wndname2, destination);
-	waitKey();
+		imshow(wndname2, destination);
+		waitKey();
 
 
- }
+	 }
 	
    return 0;
 }
