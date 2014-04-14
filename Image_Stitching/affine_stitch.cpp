3edb404323ac8fd4dc5b93931b90a1e7bdb86863
terrychenism <hahakuku@hotmail.com>
2014-04-13 20:30:02 -0400

    Update affine_stitch.cpp

--- Image_Stitching/affine_stitch.cpp
+++ Image_Stitching/affine_stitch.cpp
@@ -27,9 +27,9 @@ int main( int argc, char** argv ){
    Mat img2 =imread( "r_undistorted_perspective.jpg", 1 );
     //double w = (double)img.cols;
     //double h = (double)img.rows;
-	//double alpha = 0.5;
-int alpha_= -90., beta_= 90., gamma_=240.;
-	int f_ = 500, dist_ = 500,dx_ = 0, dy_= 0;
+    //double alpha = 0.5;
+int alpha_= 90., beta_= 90., gamma_=90.;
+    int f_ = 500, dist_ = 500,dx_ = 0, dy_= 0;
 
 
 Mat destination,destination2;
@@ -45,13 +45,13 @@ string tbarname6 = "Dx";
 string tbarname7 = "Dy";
 namedWindow(wndname1, 1);
 namedWindow(wndname2, 1);
- //createTrackbar(tbarname1, wndname1, &alpha_, 360);
- //createTrackbar(tbarname2, wndname1, &beta_, 360);
- //createTrackbar(tbarname3, wndname1, &gamma_, 360);
- //createTrackbar(tbarname4, wndname1, &f_, 2000);
- //createTrackbar(tbarname5, wndname1, &dist_, 2000);
- //createTrackbar(tbarname6, wndname1, &dx_, 2000);
- //createTrackbar(tbarname7, wndname1, &dy_, 2000);
+ createTrackbar(tbarname1, wndname1, &alpha_, 360);
+ createTrackbar(tbarname2, wndname1, &beta_, 360);
+ createTrackbar(tbarname3, wndname1, &gamma_, 360);
+ // createTrackbar(tbarname4, wndname1, &f_, 2000);
+ // createTrackbar(tbarname5, wndname1, &dist_, 2000);
+ // createTrackbar(tbarname6, wndname1, &dx_, 2000);
+ // createTrackbar(tbarname7, wndname1, &dy_, 2000);
 
 
 
@@ -59,16 +59,16 @@ namedWindow(wndname2, 1);
 while(true) {
     double f, dist , dx ,dy ;
     double alpha, beta, gamma;
-    //alpha = ((double)alpha_ - 90.)*CV_PI/180;
-    //beta = ((double)beta_ - 90.)*CV_PI/180;
-    //gamma = ((double)gamma_ - 90.)*CV_PI/180;
-	alpha =2.618;
-	beta = 0.0;
-	gamma =  -3.142;
+   alpha = ((double)alpha_ - 90.)*CV_PI/180;
+   beta = ((double)beta_ - 90.)*CV_PI/180;
+   gamma = ((double)gamma_ - 90.)*CV_PI/180;
+    // alpha =0.0;
+    // beta = -3.142;
+    // gamma = 2.618;
     f = (double) f_;
     dist = (double) dist_;
     dx = (double)dx_;
-	dy = (double)dy_;
+    dy = (double)dy_;
 
     Size taille = img.size();
     double w = (double)taille.width, h = (double)taille.height;
@@ -104,7 +104,7 @@ while(true) {
 
     Mat T = (Mat_<double>(4, 4) <<
         1, 0, 0, dx,
-		0, 1, 0, dy,
+        0, 1, 0, dy,
         0, 0, 1, dist,
         0, 0, 0, 1);
 
@@ -113,18 +113,27 @@ while(true) {
         0, f, h/2, 0,
         0, 0,   1, 0);
 
+    double a = 0.966, b=-0.259,c=0.0,d=0.0;
 
-    Mat transfo = A2 * (T * (R * A1));
+    Mat TR = (Mat_<double>(4,4) <<
+    	a*a+b*b-c*c-d*d, 2*b*c-2*a*d, 2*b*d+2*a*c, 0,
+    	2*b*c+2*a*d, a*a-b*b+c*c-d*d, 2*c*d-2*a*b,0,
+    	2*b*d-2*a*c,2*c*d+2*a*b, a*a-b*b-c*c+d*d,0,
+    	0,0,0,1);
+    
+	Mat transfo = A2 * (R *(TR*(T  *A1)));
+
+    //Mat transfo = A2 * (T * (R * A1));
 
 
     warpPerspective(img, destination, transfo, taille, INTER_CUBIC | WARP_INVERSE_MAP);
-	 //warpPerspective(img2, destination2, transfo, taille, INTER_CUBIC | WARP_INVERSE_MAP);
-   
-	imshow(wndname1, destination);
-	imshow(wndname2, img2);
-	waitKey();
+     //warpPerspective(img2, destination2, transfo, taille, INTER_CUBIC | WARP_INVERSE_MAP);
+
+    imshow(wndname1, destination);
+    imshow(wndname2, img2);
+    waitKey();
 
  }
-	
+
    return 0;
 }
