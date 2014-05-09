56e187d62fa943a287e0eace9a55c611cfb8f835
terrychenism <hahakuku@hotmail.com>
2014-05-09 18:23:36 -0400

    Update ZMTY.cpp

--- sphere_map/ZMTY.cpp
+++ sphere_map/ZMTY.cpp
@@ -60,3 +60,14 @@ void ZMTY::ZMTYfCaculate3(double newx1,double newy1)
 	y = y1-z*tan(beta_y);
 
 }
+void ZMTY::ZMTYfCaculate4(double newx1,double newy1)
+{
+	x1=newx1;
+	y1=newy1;
+	double xc = W/2;
+	double yc = H/2;
+	x=f*tan(x1/f-hfov/2)+xc;
+	//y=(y1-yc)*sqrt((x-xc)*(x-xc)+f*f)/f+yc;	
+	y=f*(y1-yc)/sqrt((x-xc)*(x-xc)+f*f)+yc;	
+	
+}
