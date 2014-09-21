2da8b68d2e471e5b4a43c648fb7aa5aa6a85646f
Terry Chen <hahakuku@hotmail.com>
2014-09-21 10:14:49 -0400

    Update read_file.cpp

--- read_file.cpp
+++ read_file.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 #include <fstream>
+
+void readFile(){
+	std::fstream myfile("abc.txt", std::ios_base::in);
+	double a;
+	double points[18][6];
+	for (int i = 0; i < 18; i++){
+		for (int j = 0; j < 6; j++){
+			myfile >> a; 
+			points[i][j] = a;			
+		}
+	}
+}
+
+
 int main(int argc, char * argv[])
 {
 	std::fstream myfile("aaa.txt", std::ios_base::in);
